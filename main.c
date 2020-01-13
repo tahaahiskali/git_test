dd95faba5915403842ad7caf19cd5974b2ea317f
Taha Ahıskalı <taha.ahiskali@tengen.com.tr>
2020-01-13 12:57:47 +0300

    main file is rearranged

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+#include <stdio.h>
+
 #include "hello.h"
+#include "add.h"
+#include "multiply.h"
 
 int main()
 {
+    int integerResult = 0;
+    float floatResult = 0;
+
     sayHello();
+    
+    printf("%d", add(5, 8));
+    printf("%f", multiply(5, 8));
+    
     return 0;
 }
