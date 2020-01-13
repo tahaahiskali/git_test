c51109d43529742a773d5b06c6a22ebb310b9df9
Taha Ahıskalı <taha.ahiskali@tengen.com.tr>
2020-01-13 12:33:56 +0300

    add and multilpy operations are added

---
+++ add.h
@@ -0,0 +1,12 @@
+#ifndef "ADD.h"
+#define "ADD.h"
+
+/**
+  * Dsc : plus any two integer number.
+  * @param1 any integer number
+  * @param2 any integer number
+  * return sum of two number
+**/
+int add( int, int );
+
+#endif
---
+++ multiply.h
@@ -0,0 +1,12 @@
+#ifndef "MULTIPLY_H"
+#define "MULTIPLY_H"
+
+/**
+  * Dsc : multiply any two float number
+  * @param1 : any float number
+  * @param2 : any float number
+  * return : multiplication of any two float number
+**/
+float multiply(float, float);
+
+#endif
